7262a3ad23cf88051b8be1de8069ac5fb52e0f5c
vanessa <vane.busato9@gmail.com>
2026-02-04 19:29:41 +0100

    sistemata navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
     return <div id="navbar">
-        <nav>
+        <nav className="links">
             <NavLink className="link" to="/">Homepage</NavLink>
             <NavLink className="link" to="/about">About Us</NavLink>
             <NavLink className="link" to="/products">Products</NavLink>

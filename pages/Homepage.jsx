70f6dc3ea2b130c085cf88a14418de36d17964f8
vanessa <vane.busato9@gmail.com>
2026-02-04 19:17:46 +0100

    inserito background homepage

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -1,6 +1,6 @@
 function Homepage() {
     return <div>
-        <img src="https://veronauno.it/wp-content/uploads/2019/08/Mixeri%CC%80-1-65_1.jpg" alt="vetrina" />
+        <img className="img-homepage" src="https://veronauno.it/wp-content/uploads/2019/08/Mixeri%CC%80-1-65_1.jpg" alt="vetrina" />
     </div>
 }
 
